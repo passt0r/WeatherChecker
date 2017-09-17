e13396d98e5a35dde940e448d4818bcd8c33f32d
passt0r <dima3@ymail.com>
2017-09-17 17:58:19 +0300

    Initial Commit

---
+++ WeatherChecker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherChecker
+//
+//  Created by Dmytro Pasinchuk on 17.09.17.
+//  Copyright © 2017 Dmytro Pasinchuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
